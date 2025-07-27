d11b18b6b6a98d64ca43607fd33de7fd723daa88
Bharath kumar_155 bharathmt15 <kumarbharat99928@gmail.com>
2025-07-27 11:16:37 +0530

    comments added for better understanding

--- Task-1/xss.js
+++ Task-1/xss.js
@@ -1,13 +1,15 @@
 window.onload = function () {
-    const container = document.getElementById("product-container");
+    const container = document.getElementById("product-container"); // html div element where the API data is fetched and shown on browser
 
-    fetch("https://dummyjson.com/products")
+    fetch("https://dummyjson.com/products") // this is the fake API used found it online
         .then((res) => res.json())
         .then((data) => {
             console.clear();
             data.products.forEach((product) => {
+                // iterating over the data and placing them into cards
                 const card = document.createElement("div");
                 card.classList.add("card");
+                // writing HTML in template literlas
                 card.innerHTML = `
           <img src="${product.thumbnail}" alt="${product.title}" />
           <div class="card-body">
