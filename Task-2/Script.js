2b1681401b9043722fe45db94c5452f43fd9081b
Bharath kumar_155 bharathmt15 <kumarbharat99928@gmail.com>
2025-07-27 11:26:55 +0530

    writing comments for better understanding

--- Task-2/Script.js
+++ Task-2/Script.js
@@ -1,9 +1,10 @@
+// this is a library installed for using the socket's in Node js
 const socket = io();
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const messages = document.getElementById("messages");
 const typingStatus = document.getElementById("typing-status");
-
+// taking vaiable as myId and later then manipulating.
 let myId = null;
 
 socket.on("connect", () => {
@@ -11,10 +12,12 @@ socket.on("connect", () => {
 });
 
 form.addEventListener("submit", (e) => {
+    // preventing the defaulut behaviour of the data and HTML elements
     e.preventDefault();
     const message = input.value.trim();
     if (message !== "") {
         displayMessage(message, true);
+        // here message is the text typed by user which everyone can view it.
         socket.emit("chat message", message);
         input.value = "";
         socket.emit("typing", false);
@@ -22,20 +25,24 @@ form.addEventListener("submit", (e) => {
 });
 
 input.addEventListener("input", () => {
+    // function will get triggered when someone starts typing in the input area
     socket.emit("typing", input.value.trim() !== "");
 });
 
 socket.on("chat message", (data) => {
+    // displays only the message when the Id is correct if it's null then the typed message is not displayed
     if (data.id !== myId) {
         displayMessage(data.msg, false);
     }
 });
 
 socket.on("typing", (isTyping) => {
+    // creates a typing animation on the area when someone starts typing
     typingStatus.textContent = isTyping ? "Typing." : "";
 });
 
 function displayMessage(message, isMine) {
+    // always creates a new space for new messages so that the messages dont get overlaped
     const div = document.createElement("div");
     div.className = "message " + (isMine ? "you" : "other");
     div.textContent = message;
--- Task-2/Server.js
+++ Task-2/Server.js
@@ -1,28 +1,39 @@
+// using express as we are building a node js project
 const express = require("express");
+// it's a library used for routing
 const path = require("path");
+// calling express function
 const app = express();
+// creating a server naming it as app
 const http = require("http").createServer(app);
+// using the socket.io library along with the http server function
 const io = require("socket.io")(http);
+// assigning a port to the server
 const PORT = 3000;
 
+// server a static page by default
 app.use(express.static(__dirname));
 
+// setting a basic pathg using the get method and servring the html file onto the server
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
 io.on("connection", (socket) => {
+    // checking for the connectiong if connection is perfectly done then we will display the message
     socket.on("chat message", (data) => {
         io.emit("chat message", {id: socket.id, msg: data});
     });
 
     socket.on("typing", (isTyping) => {
+        // checking if some one is typing or not
         socket.broadcast.emit("typing", isTyping);
     });
-
+    // when the server is disconnect we run a empty function
     socket.on("disconnect", () => {});
 });
 
 http.listen(PORT, () => {
+    // connecting a port to the server
     console.log(`Server running at http://localhost:${PORT}`);
 });
