983f468c46d608c406a4cc9448eeec53942a4838
Bharath kumar_155 bharathmt15 <kumarbharat99928@gmail.com>
2025-08-08 20:53:22 +0530

    Fetching data from API

--- Task-1/xss.js
+++ Task-1/xss.js
@@ -2,14 +2,15 @@ window.onload = function () {
     const container = document.getElementById("product-container"); // html div element where the API data is fetched and shown on browser
 
     fetch("https://dummyjson.com/products") // this is the fake API used found it online
-        .then((res) => res.json())
+        .then((res) => res.json()) // converting the API data to JSON
         .then((data) => {
+            // hadling the data
             console.clear();
             data.products.forEach((product) => {
                 // iterating over the data and placing them into cards
                 const card = document.createElement("div");
                 card.classList.add("card");
-                // writing HTML in template literlas
+                // writing HTML in template literals(Back ticks) to manipulate the DOM
                 card.innerHTML = `
           <img src="${product.thumbnail}" alt="${product.title}" />
           <div class="card-body">
